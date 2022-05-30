17cee0f62475d26abdbd3c1de6ec28b58028c05e
Muhammad Ramadhan <mayatcopo@gmail.com>
2022-05-30 20:59:12 +0700

    Menambahkan fitur Custom Spinner

--- app/src/main/java/com/example/customdialogspinner_muhammadramadhan/MainActivity.kt
+++ app/src/main/java/com/example/customdialogspinner_muhammadramadhan/MainActivity.kt
@@ -6,9 +6,26 @@ import android.widget.*
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    val nama = arrayOf(
+        "Kota Pangkalpinang", "Kabupaten Bangka", "Kabupaten Bangka Barat",
+        "Kabupaten Bangka Tengah", "Kabupaten Bangka Selatan", "Kabupaten Belitung",
+        "Kabupaten Belitung Timur"
+    )
+    val logo = intArrayOf(
+        R.drawable.logo_pangkalpinang, R.drawable.logo_bangka, R.drawable.logo_babar,
+        R.drawable.logo_bateng, R.drawable.logo_basel, R.drawable.logo_belitung,
+        R.drawable.logo_beltim
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spnKabKota = findViewById<Spinner>(R.id.spnKabKota)
+
+        val adpKabKota =  AdapterKabKota(this@MainActivity, logo, nama)
+        spnKabKota.adapter = adpKabKota
+
 
         val btnKenalan = findViewById<Button>(R.id.btnKenalan)
 
